cb62ab2a0145aa28b30f3fe6874d1fd20b29fdb6
peddih9242 <78389729+peddih9242@users.noreply.github.com>
2021-06-28 15:11:23 +1200

    first commit

---
+++ L1_Database_Support_Files/index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE HTML>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Book Review Database">
+    <meta name="keywords" content="books, reading, fiction, non-fiction, genre, reviews">
+    <meta name="author" content="Harish Peddi">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Book Review Database</title>
+    
+    <!-- Edit the link below / replace with your chosen google font -->
+    <link href="https://fonts.googleapis.com/css?family=Lato%7cUbuntu" rel="stylesheet"> 
+    
+    <!-- Edit the name of your style sheet - 'foo' is not a valid name!! -->
+    <link rel="stylesheet" href="no_side.css"> 
+    
+</head>
+    
+<body>
+    
+    <div class="wrapper">
+    
+        <div class="box banner">
+            
+        <!-- logo image linking to home page goes here -->
+        <a href="index.php">
+            <div class="box logo"  title="Logo - Click here to go to the Home Page">
+            <img class="img-circle" src="images/gen_logo.png" width="150" height="150" alt="generic logo" />
+            
+            </div>    <!-- / logo -->
+        </a>
+            
+            <h1>Orchid Reading</h1>
+        </div>    <!-- / banner -->
+        
+        <div class="side">
+        
+        <h2>Side Bar</h2>
+            <p>items</p>
+        </div> <!-- / side bar -->
+
+        <div class="box main"> 
+            <h2>First Page heading</h2>
+            
+            <p>
+                Sugar plum powder dessert bonbon powder lollipop lollipop muffin I love. Tiramisu dessert I love sugar plum apple pie marzipan chocolate bar brownie. Pudding tiramisu caramels biscuit tiramisu. I love gummies gummies pie sweet pudding I love I love. Donut halvah candy oat cake donut. Caramels wafer danish jelly-o.
+            </p>
+            
+            <p>
+                Jelly beans gingerbread jelly tootsie roll. Lemon drops bear claw I love tiramisu candy canes cake. Jelly-o apple pie powder tart I    love candy tiramisu. Topping liquorice cake sesame snaps brownie. Lemon drops jelly beans brownie danish I love fruitcake cotton candy danish. Dragée pastry halvah macaroon I love chocolate bar caramels liquorice. Halvah cupcake muffin fruitcake pastry I love bear claw I love jelly. Tart chocolate bar croissant danish jelly. Sugar plum powder cupcake muffin gingerbread I love sweet.
+            </p>
+            
+            <p>
+                Biscuit jelly I love jelly-o candy canes chupa chups topping lemon drops I love. Sesame snaps macaroon lemon drops carrot cake I love. Chocolate bar gingerbread chupa chups. Dessert chocolate pastry liquorice tootsie roll lemon drops sweet macaroon liquorice. I love muffin bonbon. Cupcake apple pie chocolate cake bonbon topping. Macaroon gummi bears dragée caramels donut chocolate.
+            </p>
+            
+        </div>    <!-- / main -->
+        
+
+        <div class="box footer">
+            CC Harish Peddi 2021
+        </div>    <!-- / footer -->
+    
+    </div>  <!-- / wrapper  -->
+    
+</body>
